4150846d6a9455b26c1deb9f25d9ca3254fcd516
Mário de Lima <mariodelimadobrasil@gmail.com>
2023-03-13 08:21:28 -0300

    Create update-remover-espacos-do-inicio-e-do-fim-da-coluna.sql

---
+++ update-remover-espacos-do-inicio-e-do-fim-da-coluna.sql
@@ -0,0 +1,11 @@
+UPDATE colaboradores
+SET nome = TRIM(nome);
+
+UPDATE colaboradores
+SET cargo = TRIM(cargo);
+
+UPDATE colaboradores
+SET local = TRIM(local);
+
+UPDATE colaboradores
+SET vinculo = TRIM(vinculo);
